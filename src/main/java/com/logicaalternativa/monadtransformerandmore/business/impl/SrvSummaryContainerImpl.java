835633bedfee1eeb00b9f21e0eecdaef62e6c0fe
sucius <alvaro.pozo.cuenca@gmail.com>
2018-03-19 12:04:26 +0100

    Merge branch 'trainingForTrainer' into workshop_usingMonadError
    # Conflicts:
    #	src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
    #	src/main/java/com/logicaalternativa/monadtransformerandmore/monad/impl/MonadFutEitherError.java
    #	src/test/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutEitherImpTest.java

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
@@ -2,10 +2,18 @@ package com.logicaalternativa.monadtransformerandmore.business.impl;
 
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
 
-import com.logicaalternativa.monadtransformerandmore.bean.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.logicaalternativa.monadtransformerandmore.bean.Author;
+import com.logicaalternativa.monadtransformerandmore.bean.Book;
+import com.logicaalternativa.monadtransformerandmore.bean.Chapter;
+import com.logicaalternativa.monadtransformerandmore.bean.Sales;
+import com.logicaalternativa.monadtransformerandmore.bean.Summary;
 import com.logicaalternativa.monadtransformerandmore.business.SrvSummaryContainer;
 import com.logicaalternativa.monadtransformerandmore.container.Container;
-import com.logicaalternativa.monadtransformerandmore.errors.impl.MyError;
 import com.logicaalternativa.monadtransformerandmore.monad.MonadContainer;
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceAuthorContainer;
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceBookContainer;
@@ -13,62 +21,51 @@ import com.logicaalternativa.monadtransformerandmore.service.container.ServiceCh
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceSalesContainer;
 import com.logicaalternativa.monadtransformerandmore.errors.Error;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 public class SrvSummaryContainerImpl implements SrvSummaryContainer<Error> {
 
-    private final ServiceBookContainer<Error> srvBook;
-    private final ServiceSalesContainer<Error> srvSales;
-    private final ServiceChapterContainer<Error> srvChapter;
-    private final ServiceAuthorContainer<Error> srvAuthor;
+	private final ServiceBookContainer<Error> srvBook;
+	private final ServiceSalesContainer<Error> srvSales;
+	private final ServiceChapterContainer<Error> srvChapter;
+	private final ServiceAuthorContainer<Error> srvAuthor;
 
-    private final MonadContainer<Error> m;
-
-
-    public SrvSummaryContainerImpl(ServiceBookContainer<Error> srvBook,
-                                   ServiceSalesContainer<Error> srvSales,
-                                   ServiceChapterContainer<Error> srvChapter,
-                                   ServiceAuthorContainer<Error> srvAuthor, MonadContainer<Error> m) {
-        super();
-        this.srvBook = srvBook;
-        this.srvSales = srvSales;
-        this.srvChapter = srvChapter;
-        this.srvAuthor = srvAuthor;
-        this.m = m;
-    }
+	private final MonadContainer<Error> m;
 
 
-    @Override
-    public Container<Error, Summary> getSummary(Integer idBook) {
-
-        Container<Error, Book> book = srvBook.getBook(idBook);
-        Container<Error, Sales> sales = srvSales.getSales(idBook);
-
-
-        if (book.isOk()) {
-
-            Container<Error, Author> author = srvAuthor.getAuthor(book.getValue().getIdAuthor());
-
-            if (author.isOk()) {
-
-                if (book.getValue().getChapters().stream().allMatch(c -> srvChapter.getChapter(c).isOk())) {
-                    List<Chapter> chapters =
-                            book.getValue().getChapters().stream().map(c -> srvChapter.getChapter(c).getValue()).collect(Collectors.toList());
-
-                    Summary summary = new Summary(book.getValue(),
-                            chapters, sales.isOk() ? Optional.of(sales.getValue()) : Optional.empty(),
-                            author.isOk() ? author.getValue() : null);
-
-                    return Container.value(summary);
-                }
-            }
-        }
-
-        return Container.error(new MyError("It is impossible to get book summary"));
+	public SrvSummaryContainerImpl(ServiceBookContainer<Error> srvBook,
+			ServiceSalesContainer<Error> srvSales,
+			ServiceChapterContainer<Error> srvChapter,
+			ServiceAuthorContainer<Error> srvAuthor, MonadContainer<Error> m) {
+		super();
+		this.srvBook = srvBook;
+		this.srvSales = srvSales;
+		this.srvChapter = srvChapter;
+		this.srvAuthor = srvAuthor;
+		this.m = m;
+	}
 
 
-    }
+
+	@Override
+	public Container<Error, Summary> getSummary(Integer idBook) {
+
+		final Container<Error, Book> bookC = srvBook.getBook( idBook );
+
+		final Container<Error, Sales> salesC = srvSales.getSales( idBook );
+
+		final Container<Error, Author> authorC = srvAuthor.getAuthor( bookC.getValue().getIdAuthor() );
+
+		List<Chapter> listChapter = bookC.getValue().getChapters().stream()
+		  .map( idChapter -> srvChapter.getChapter(idChapter).getValue()  )
+		  .collect(Collectors.toList());
+
+		final Summary summary = new Summary(bookC.getValue(),
+								listChapter,
+								Optional.of(salesC.getValue()),
+								authorC.getValue());
+
+
+		return Container.value(summary);
+
+	}
 
 }
