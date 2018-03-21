a6a98e7474963148541286a6563448be13091d4e
sucius <alvaro.pozo.cuenca@gmail.com>
2018-03-21 13:29:35 +0100

    Merge branch 'trainingForTrainer' into workshop_usingMonadError
    # Conflicts:
    #	src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
    #	src/main/java/com/logicaalternativa/monadtransformerandmore/monad/impl/MonadFutEitherError.java
    #	src/test/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutEitherImpTest.java

--- src/main/scala/business/SrvSummaryF.scala
+++ src/main/scala/business/SrvSummaryF.scala
@@ -10,49 +10,44 @@ import monad.syntax.Implicits._
 import java.util.Optional
 import scala.collection.JavaConverters._
 
-trait SrvSummaryF[E,P[_]] {
-  
-  implicit val E : Monad[E,P]
-  
+trait SrvSummaryF[E, P[_]] {
+
+  implicit val E: Monad[E, P]
+
   import E._
-  
-  val srvBook : ServiceBookF[E,P]
-  val srvSales : ServiceSalesF[E,P]
-  val srvChapter : ServiceChapterF[E,P]
-  val srvAuthor : ServiceAuthorF[E,P]
-  
-  def getSummary( idBook: Int) : P[Summary] = {
-    
 
-    val bookP = srvBook.getBook( idBook )
-    val salesP = srvSales.getSales( idBook )
-                  .map( Optional.of(_) )
-                  .recover( _ => Optional.empty[Sales] )
+  val srvBook: ServiceBookF[E, P]
+  val srvSales: ServiceSalesF[E, P]
+  val srvChapter: ServiceChapterF[E, P]
+  val srvAuthor: ServiceAuthorF[E, P]
+
+  def getSummary(idBook: Int): P[Summary] = {
 
 
-
-    for{
-
-        sales  <- salesP
-        book   <- bookP
-        author <- srvAuthor.getAuthor( book.getIdAuthor )
-        listC  <- sequence(
-                    book.getChapters.asScala.toList.map{
-                      ch => srvChapter.getChapter( ch )
-                    }
-                  )
-
-    } yield( new Summary( book, listC.asJava, sales, author ) )
+    val bookP = srvBook.getBook(idBook)
+    val salesP = srvSales.getSales(idBook)
+      .map(Optional.of(_))
 
 
-    //~ srvBook.getBook( idBook ).flatMap( (book : Book) => ??? )
+    val res = for {
+      sales <- salesP
+      book <- bookP
+      summary <- createSummary(book, sales)
+    } yield summary
 
+    res.recoverWith(_ => raiseError(getGenericError("It is impossible to get book summary")))
+  }
 
+  private def createSummary(book: Book, sales: Optional[Sales]): P[Summary] = {
+    val authorP = srvAuthor.getAuthor(book.getIdAuthor)
+    val listP = sequence(book.getChapters.asScala.toList.map(ch => srvChapter.getChapter(ch)))
 
-    ???
-    
-  } 
-  
-  protected[SrvSummaryF] def getGenericError( s : String ) : E
-  
+    for {
+      author <- authorP
+      listC <- listP
+    } yield new Summary(book, listC.asJava, sales, author)
+  }
+
+  protected[SrvSummaryF] def getGenericError(s: String): E
+
 }
